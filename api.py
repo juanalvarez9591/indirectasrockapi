52966ed0842dee0081934287b777d8bf0869dc24
Juancho <juanalvarez9591@gmail.com>
2021-08-11 16:43:37 -0300

    A

--- api.py
+++ api.py
@@ -1,26 +1,25 @@
+from flask import Flask, render_template
 import json
 import random
-import os
-from flask import Flask
-from flask.helpers import send_from_directory
-from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
 
 
-class IndirectaRock(Resource):
-    def get(self):
-        f = open("indirectas.json")
-        x = json.load(f)
-        length = len(x)-1
+@app.route('/indirecta', methods=['GET'])
+def get():
+    f = open("indirectas.json")
+    x = json.load(f)
+    length = len(x)-1
 
-        randindex = random.randint(0, length)
+    randindex = random.randint(0, length)
 
-        return(str(x[randindex][0]))
+    return(str(x[randindex][0]))
 
 
-api.add_resource(IndirectaRock, '/')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
