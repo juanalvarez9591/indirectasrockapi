9cae82962c202ef10c1a381be5cdef28dfe37aaf
Juancho <juanalvarez9591@gmail.com>
2021-08-12 20:12:14 -0300

    Debo saber si es verdad

--- api.py
+++ api.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask, render_template
 import json
 import random
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/indirecta', methods=['GET'])
